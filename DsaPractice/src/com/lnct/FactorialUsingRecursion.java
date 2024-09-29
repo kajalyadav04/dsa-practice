953abac906e3eb3fa99425b6f069e24cafb590b4
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-29 18:53:43 +0530

    factorial program optimized solution  using recursion

--- DsaPractice/src/com/lnct/FactorialUsingRecursion.java
+++ DsaPractice/src/com/lnct/FactorialUsingRecursion.java
@@ -7,8 +7,16 @@ public class FactorialUsingRecursion {
 		int n=6;
 		int fact=1;
 		factorialNumber(n,fact);
-		
+		int ans = factorialNumberV2(n);
+		//5*fact(4)*fact(3)*fact(2)*
+		System.out.println("factrial: "+ans);
+	}
 
+	private static int factorialNumberV2(int n) {
+		if(n==0) {
+			return n;
+		}
+		return n*factorialNumberV2(n-1);
 	}
 
 	private static void factorialNumber(int n,int fact) {
@@ -21,9 +29,10 @@ public class FactorialUsingRecursion {
 		
 		
 		fact=fact*n;
-//		System.out.println(fact);
+		System.out.println(fact);
 		factorialNumber(n-1,fact);
 		
 	}
+	
 
 }
