f58ec92387e531c490485bce8d4b3c0e91d3039c
kajalyadav04 <kajalraj1520@gmail.com>
2024-09-30 12:27:07 +0530

    sum of natural numbers without using formula

--- DsaPractice/src/com/lnct/NaturalNumSum.java
+++ DsaPractice/src/com/lnct/NaturalNumSum.java
@@ -7,7 +7,18 @@ public class NaturalNumSum {
 		int n=6;
 		int res=naturalNumSumUsingFormula(n);
 		System.out.println(res);
+		int k=naturalNumSumUsingLogic(n);
+		System.out.println(k);
 
+	}
+
+	private static int naturalNumSumUsingLogic(int n) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			sum+=i;
+		}
+		return sum;
 	}
 
 	private static int naturalNumSumUsingFormula(int n) {
