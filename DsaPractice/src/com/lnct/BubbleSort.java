5c2ed830ae8ff66360decdfbf73a8d531c22ed86
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-01 15:08:54 +0530

    bubble sort

---
+++ DsaPractice/src/com/lnct/BubbleSort.java
@@ -0,0 +1,40 @@
+package com.lnct;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int[] arr= {36,78,22,11,6,5,78};
+     int n=arr.length;
+    int[] res= sort(arr,n);
+    
+    System.out.println(Arrays.toString(res));
+     
+	}
+
+	private static int[] sort(int[] arr, int n) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < n - 1; i++) {
+			boolean flag=false;
+			for (int j = 0; j < n-1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=temp;
+                        flag=true;
+				}
+				
+			}
+			
+			if(flag==false) {
+				return arr;
+			}
+		}
+		return arr;
+
+	}
+
+}
