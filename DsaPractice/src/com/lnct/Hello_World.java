b25e8f022fb016bb92c76bc9d07cab48ca3679a7
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-03 00:57:46 +0530

    hello world program hackerrank

---
+++ DsaPractice/src/com/lnct/Hello_World.java
@@ -0,0 +1,10 @@
+package com.lnct;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World." + '\n' + "Hello, Java.");
+	}
+
+}
