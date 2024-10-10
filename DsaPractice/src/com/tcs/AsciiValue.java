7f94b494c72e78d39c4f0f6dbea1ca2a055b329a
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-10 15:36:46 +0530

    print ascii value

---
+++ DsaPractice/src/com/tcs/AsciiValue.java
@@ -0,0 +1,18 @@
+package com.tcs;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         char ch='A';
+         asciifn(ch);
+	}
+
+	private static void asciifn(char ch) {
+		// TODO Auto-generated method stub
+		int k=(int)ch;
+		System.out.println(k);
+		
+	}
+
+}
