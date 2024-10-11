626de851b6b1c33ac6bcf429d1032959f4e4c877
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-11 16:40:23 +0530

    is palindrome

---
+++ DsaPractice/src/com/leetcode/IsPalindronme.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+public class IsPalindronme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=121;
+		boolean k=isPalindrome(x);
+		System.out.println(k);
+
+	}
+	public static boolean isPalindrome(int x) {
+        int res=0;
+        int num=x;
+        while(x>0){
+            int rem=x%10;
+             res=res*10+rem;
+            x=x/10;
+        }
+        if(num==res){
+            return true;
+        }
+        return false;
+    }
+
+}
