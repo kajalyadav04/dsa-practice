cee0718ebc7809e170b41afc2f8df784a80d6173
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-18 13:58:26 +0530

    remove particular ele

--- DsaPractice/src/com/leetcode/RemoveDuplicate.java
+++ DsaPractice/src/com/leetcode/RemoveDuplicate.java
@@ -1,5 +1,21 @@
 package com.leetcode;
 
+import java.util.HashSet;
+
 public class RemoveDuplicate {
+
+	public int removeDuplicates(int[] nums) {
+		HashSet<Integer> s = new HashSet<>();
+
+		int j = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (!s.contains(nums[i])) {
+				s.add(nums[i]);
+				nums[j++] = nums[i];
+			}
+
+		}
+		return j;
+	}
 
 }
---
+++ DsaPractice/src/com/leetcode/RemoveParticularNum.java
@@ -0,0 +1,10 @@
+package com.leetcode;
+
+public class RemoveParticularNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
