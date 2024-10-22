f89dd1cd225171a4d41a69afe740276cf8bec5cf
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-22 19:41:34 +0530

    find target leetcode find target problem

--- DsaPractice/src/com/leetcode/FindTheIndexOfString.java
+++ DsaPractice/src/com/leetcode/FindTheIndexOfString.java
@@ -2,9 +2,30 @@ package com.leetcode;
 
 public class FindTheIndexOfString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	    public int strStr(String haystack, String needle) {
+	    //     if(haystack.length()==needle.length()){
+	    //         return 0;
+	    //     }
+	    //     for(int i=0;i<haystack.length()-needle.length();i++){
+	    //         if(haystack.charAt(i)==needle.charAt(0)){
+	    //             if(haystack.substring(i,needle.length()+i).equals(needle)){
+	    //                 return i;
+	    //             }
+	    //             else{
+	    //                 return -1;
+	    //             }
+	    //         }
+	            
+	    //     }
+	    // return -1;
+	    if(haystack.contains(needle)){
+	        return haystack.indexOf(needle);
+	    }
+	    else{
+	        return -1;
+	    }
 	}
+	
 
 }
