bdabd2f17567836e289d056cc405908c5c5aa6ec
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-27 23:52:28 +0530

    one plus

--- DsaPractice/src/com/leetcode/FindTarget.java
+++ DsaPractice/src/com/leetcode/FindTarget.java
@@ -2,4 +2,18 @@ package com.leetcode;
 
 public class FindTarget {
 
+	public int searchInsert(int[] nums, int target) {
+		int i = 0;
+		for (i = 0; i < nums.length; i++) {
+			if (nums[i] <= target) {
+				if (nums[i] == target) {
+					return i;
+				}
+			} else {
+				return i;
+			}
+		}
+		return i;
+
+	}
 }
