47b71b3187b00c9b7ab0fb280a735c59f71cfac2
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-28 23:25:53 +0530

    one plus prlm leetcode

--- DsaPractice/src/com/leetcode/OnePlus.java
+++ DsaPractice/src/com/leetcode/OnePlus.java
@@ -1,5 +1,23 @@
 package com.leetcode;
 
 public class OnePlus {
+	
+	    public int[] plusOne(int[] digits) {
+	       
+	        int k=digits.length;
+	        for(int i=k-1;i>=0;i--){
+	            if(digits[i]<9){
+	                digits[i]++;
+	                return digits;
+	            }
+	                digits[i]=0;
+	        }
+	        int[] newNumber = new int[k+1];
+	        newNumber[0]=1;
+	       
+	        return newNumber;
+	        
+	    }
+	
 
 }
