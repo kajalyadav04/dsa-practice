ee0d2a36929f4ecffa3877c7036e76ec60f30941
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-30 23:53:59 +0530

    find target

--- DsaPractice/src/com/leetcode/FindTarget.java
+++ DsaPractice/src/com/leetcode/FindTarget.java
@@ -2,18 +2,24 @@ package com.leetcode;
 
 public class FindTarget {
 
-	public int searchInsert(int[] nums, int target) {
-		int i = 0;
+	public static int searchInsert(int[] nums, int target) {
+		int i;
 		for (i = 0; i < nums.length; i++) {
-			if (nums[i] <= target) {
-				if (nums[i] == target) {
-					return i;
-				}
-			} else {
+
+			if (nums[i] == target) {
 				return i;
 			}
 		}
+
 		return i;
+
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 8, 6, 2, 3, 7 };
+		int target = 3;
+		int k = searchInsert(nums, target);
+		System.out.println(k);
 
 	}
 }
