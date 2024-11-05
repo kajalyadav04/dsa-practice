a0fc78dbd3231ce74c05d139aedf7afae5ad9bea
kajalyadav04 <kajalraj1520@gmail.com>
2024-11-05 12:15:54 +0530

    remove char from alpha using regrex

--- DsaPractice/src/com/tcs/RemoveCharExceptAlpa.java
+++ DsaPractice/src/com/tcs/RemoveCharExceptAlpa.java
@@ -1,5 +1,33 @@
 package com.tcs;
 
-public class RemoveCharExceptAlpa {
+import java.util.Scanner;
 
-}
+public class RemoveCharExceptAlpa {
+	
+	    
+	    public static void main(String[] args){
+	        Scanner sc= new Scanner(System.in);
+	        String s=sc.nextLine();
+	       String k= removeVr(s);
+	       System.out.println(k);
+	       String kString= removeChRegrex(s);
+	       System.out.println(kString);
+	       
+	    }
+	    private static String removeChRegrex(String s) {
+			// TODO Auto-generated method stub
+			return s.replaceAll("[^a-zA-Z]", "");
+		}
+		public static String removeVr(String s){
+	        String l="";
+	       for(int i=0;i<s.length();i++){
+	           char ch=s.charAt(i);
+	           if(Character.isLetter(ch)){
+	               l=l+ch;
+	           }
+	       }
+	       return l;
+	    }
+	}
+
+
