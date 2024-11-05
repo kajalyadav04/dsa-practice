70c85da9e48d05767d9f4624ba929c6ad13eb9c8
kajalyadav04 <kajalraj1520@gmail.com>
2024-11-05 12:11:28 +0530

    remove char from alpha

--- DsaPractice/src/com/tcs/RemoveVowel.java
+++ DsaPractice/src/com/tcs/RemoveVowel.java
@@ -6,7 +6,7 @@ public class RemoveVowel {
 	
 	    public static void main(String args[]){
 	        Scanner sc=new Scanner(System.in);
-	        String s=sc.next();
+	        String s=sc.nextLine();
 	        String p= removeVowel(s);
 	        System.out.println(p);
 	        
