a5b7d4064b84df3c303b2cf76a29399f3cd0a28f
kajalyadav04 <kajalraj1520@gmail.com>
2024-11-18 21:35:56 +0530

    rev num

--- DsaPractice/src/com/tcs/ReverseArr.java
+++ DsaPractice/src/com/tcs/ReverseArr.java
@@ -1,5 +1,27 @@
 package com.tcs;
+import java.util.*;
 
 public class ReverseArr {
+	
+
+	
+		public static void main(String[] args){
+		    Scanner sc= new Scanner(System.in);
+		    int n=sc.nextInt();
+		    int[] arr=new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i]=sc.nextInt();
+		    }
+		    int[] arrays=reverese(arr);
+		    System.out.println(Arrays.toString(arrays));
+		}
+		public static int[] reverese(int[] arr){
+		    int[] array=new int[arr.length];
+		    for(int i=0;i<arr.length;i++){
+		       array[i]=arr[arr.length-1-i];
+		    }
+		    return array;
+		}
+	
 
 }
