db305e2486c129aeeb740631e0c385d541bcf74e
kajalyadav04 <kajalraj1520@gmail.com>
2024-11-18 21:10:22 +0530

    reverse array

--- DsaPractice/src/com/tcs/CheckPerfectNum.java
+++ DsaPractice/src/com/tcs/CheckPerfectNum.java
@@ -1,5 +1,19 @@
 package com.tcs;
 
 public class CheckPerfectNum {
+	public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++){
+            
+            if(num%i==0){
+                sum=sum+i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+        
+    }
 
 }
