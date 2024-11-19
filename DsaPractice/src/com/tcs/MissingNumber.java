ac8fbb19a9174cf48e84028e04508a3758e6a133
kajalyadav04 <kajalraj1520@gmail.com>
2024-11-19 13:51:43 +0530

    fib num

--- DsaPractice/src/com/tcs/MissingNumber.java
+++ DsaPractice/src/com/tcs/MissingNumber.java
@@ -1,5 +1,13 @@
 package com.tcs;
 
 public class MissingNumber {
+	public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int num:nums){
+          sum=sum+num;
+        }
+        return n*(n+1)/2 -sum;
+    }
 
 }
