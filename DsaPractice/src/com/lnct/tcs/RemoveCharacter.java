f43b1ae96a0ad3a68c5208bd7e66a75b5d7abdf4
kajalyadav04 <kajalraj1520@gmail.com>
2025-03-09 20:19:40 +0530

    remove special all character using logic

--- DsaPractice/src/com/lnct/tcs/RemoveCharacter.java
+++ DsaPractice/src/com/lnct/tcs/RemoveCharacter.java
@@ -1,10 +1,39 @@
 package com.lnct.tcs;
 
+
+
 public class RemoveCharacter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String string="\"$Gee*k;s..fo, r'Ge^eks?\"";
+	
+	}
+	public static String removeSpecialCharacter(String s) {
+        // code here
+        String k=s.replaceAll("[^A-Za-z]","");
+        if(k.length()==0){
+            return "-1";
+        }
+        else{
+           return k;
+        }
+    }
+	public static String removeSpecialChracterLogic(String s) {
+		 char[] arr=s.toCharArray();
+			StringBuilder str= new StringBuilder();
+			for(int i=0;i<arr.length;i++) {
+				if ((arr[i]>='a'&&arr[i]<='z')||(arr[i]>='A'&&arr[i]<='Z') ){
+					str.append(arr[i]);
+					
+				}
+			}
+			if(str.length()==0){
+			    return "-1";
+			}
+			return str.toString();
+			
+		
 	}
 
 }
