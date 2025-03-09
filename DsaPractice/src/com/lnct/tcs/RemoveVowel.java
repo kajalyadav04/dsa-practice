5b85b1ce57d4f91da86e5dbf79e5b1afe91da524
kajalyadav04 <kajalraj1520@gmail.com>
2025-03-09 07:58:53 +0530

    remove vowel using stringbuilder

--- DsaPractice/src/com/lnct/tcs/RemoveVowel.java
+++ DsaPractice/src/com/lnct/tcs/RemoveVowel.java
@@ -1,5 +1,49 @@
 package com.lnct.tcs;
 
+import java.util.Scanner;
+
 public class RemoveVowel {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String string=scanner.nextLine();
+		String a= removeVowels(string);
+		System.out.println(a);
+		
+		
+	}
+	
+	   public static String removeVowels(String s) {
+	        // code here
+	        String str="";
+	        for(int i=0;i<s.length();i++){
+	            char ch=s.charAt(i);
+	            if("aeiou".indexOf(ch)!=-1){
+	                continue;
+	            }
+	            else {
+	                str+=ch;
+	            }
+	            
+	            
+	        }
+	        return str;
+	    }
+	  public static  String removeVowelsusingStringBuikder(String s) {
+	        // code here
+	        StringBuilder str = new StringBuilder();
+	        for(int i=0;i<s.length();i++){
+	            char ch=s.charAt(i);
+	            if("aeiou".indexOf(ch)!=-1){
+	                continue;
+	            }
+	            else {
+	                str.append(ch);
+	            }
+	            
+	            
+	        }
+	        return str.toString();
+	    }
+	
 
 }
