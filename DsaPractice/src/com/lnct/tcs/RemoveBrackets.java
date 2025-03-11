f78580fcd64b677bb94338ca43715ee10eedf354
kajalyadav04 <kajalraj1520@gmail.com>
2025-03-11 13:57:02 +0530

    reomve brackets using char aaray

--- DsaPractice/src/com/lnct/tcs/RemoveBrackets.java
+++ DsaPractice/src/com/lnct/tcs/RemoveBrackets.java
@@ -2,11 +2,13 @@ package com.lnct.tcs;
 
 import java.util.Scanner;
 
+
+
 public class RemoveBrackets {
 	public static void main(String[] args) {
 		Scanner scanner= new Scanner(System.in );
 		String s= scanner.next();
-		String k=removeInbuild(s);
+		String k=removeLogic(s);
 		System.out.println(k);
 	}
 	public static String remove(String f) {
@@ -30,6 +32,34 @@ public class RemoveBrackets {
 	public static String removeInbuild(String f) {
 //		return f.replaceAll("[\\Q()[]{}\\E]", "");
 		return f.replaceAll("[\\[\\]{}()]", "");
+		
+	}
+	public static String removeLogic(String s) {
+		StringBuilder kBuilder= new StringBuilder();
+		for(char ch:s.toCharArray()) {
+			if(ch=='('||ch==')'||ch=='{'||ch=='}'||ch=='['||ch==']') {
+				continue;
+			}
+			else {
+				kBuilder.append(ch);
+			}
+		}
+		return kBuilder.toString();
+	}
+	public static String  removeBracket(String s) {
+		char[] arr= s.toCharArray();
+		StringBuilder kBuilder=new StringBuilder();
+		for(int i=0;i<arr.length;i++) {
+			
+			if(arr[i]=='('||arr[i]==')'||arr[i]=='{'||arr[i]=='}'||arr[i]=='['||arr[i]==']') {
+				continue;
+			}
+			else {
+				kBuilder.append(arr[i]);
+			}
+		}
+		
+		return kBuilder.toString();
 		
 	}
 }
