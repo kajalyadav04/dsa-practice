0d0c498ef5aec97ea0813546d43fbef1cb20dce5
kajalyadav04 <kajalraj1520@gmail.com>
2025-03-12 10:22:53 +0530

    find sum using logic

--- DsaPractice/src/com/lnct/tcs/SumOfNumberInString.java
+++ DsaPractice/src/com/lnct/tcs/SumOfNumberInString.java
@@ -1,5 +1,31 @@
 package com.lnct.tcs;
 
 public class SumOfNumberInString {
+	 // Function to calculate sum of all numbers present in a string.
+    public static int findSum(String s) {
+        // your code here
+        int sum=0;
+        String num="";
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+           
+            if(Character.isDigit(ch)){
+                num+=ch;
+            }else{
+                if(!num.equals("")){
+                    sum+=Integer.parseInt(num);
+                    num="";
+                }
+                
+            }
+            
+            
+            
+        }
+        if (!num.equals("")) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
+    }
 
 }
